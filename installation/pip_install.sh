e45da4ab42623c41c441c61d6004a60624f56331
Pandiarajan Ganapathysamy <pandiarajanxg@gmail.com>
2019-10-07 10:48:25 +0530

    Update pip_install.sh

--- installation/pip_install.sh
+++ installation/pip_install.sh
@@ -2,3 +2,6 @@ sudo yum install epel-release
 sudo yum install python-pip
 pip --version
 
+#install pip3
+yum install https://centos7.iuscommunity.org/ius-release.rpm
+yum install python36u-devel python36u-p
