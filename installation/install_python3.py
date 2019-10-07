70a247e38f55e9d15ae6366a8675288e87aa72c4
Pandiarajan Ganapathysamy <pandiarajanxg@gmail.com>
2019-10-07 10:20:23 +0530

    Update install_python3.py

--- installation/install_python3.py
+++ installation/install_python3.py
@@ -3,5 +3,14 @@
  sudo yum install centos-release-scl
  sudo yum install epel-release
  sudo yum install rh-python36
+ python3.6 -V
+ 
+ #Alternate for above steps
+ yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+ yum -y install python36u
+ python3.6 -V
+ 
  alias python "/usr/sbin/python36"
+ #or
+ alias python="/usr/bin/python36"
 
