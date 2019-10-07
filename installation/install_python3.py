e521888c4689be6a84ae06509231a02bbbd29b41
Pandiarajan Ganapathysamy <pandiarajanxg@gmail.com>
2019-10-07 10:00:32 +0530

    Update install_python3.py

--- installation/install_python3.py
+++ installation/install_python3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+ sudo yum install http://mirror.centos.org/centos/7/extras/x86_64/Packages/centos-release-scl-rh-2-3.el7.centos.noarch.rpm
  sudo yum install centos-release-scl
  sudo yum install epel-release
  sudo yum install rh-python36
