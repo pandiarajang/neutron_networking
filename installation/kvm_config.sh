3c80ce7cee90e6a481140dbe9edae418f4a6cfa6
Pandiarajan Ganapathysamy <pandiarajanxg@gmail.com>
2019-10-07 09:25:31 +0530

    Update kvm_config.sh

--- installation/kvm_config.sh
+++ installation/kvm_config.sh
@@ -1,3 +1,4 @@
+# RPM packages for centos can be searched in https://pkgs.org/download/librte_ring.so.2(DPDK_2.0)(64bit) 
 # 1) Install liblz4--dependency for spice server
 yum install http://mirror.centos.org/centos/7/os/x86_64/Packages/lz4-1.7.5-3.el7.x86_64.rpm
 # 2) Install spice server  
